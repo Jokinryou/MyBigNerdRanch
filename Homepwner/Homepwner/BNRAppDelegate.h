4e458f12dba139b82ff2349c07883341cfc2c55d
Jokinryou Tsui <jokinryou.tsui@gmail.com>
2014-04-19 13:50:13 +0800

    start chapter 8

---
+++ Homepwner/Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Jokinryou Tsui on 4/19/14.
+//  Copyright (c) 2014 Jokinryou Tsui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
